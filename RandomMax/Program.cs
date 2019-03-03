ecf0e34ad5d70f41b056ddb4365da5b13c324a9b
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-03-02 20:09:35 -0600

    100

--- RandomMax/Program.cs
+++ RandomMax/Program.cs
@@ -8,6 +8,16 @@ namespace RandomMax
         {
             // TODO: Generate a list of 100 random integers between 0 and 1000 and ouput the maximum value.
 
+            int randomInt = new Random(100).Next(0, 1001);
+            
+            int maxValue = randomInt++;
+
+            if (randomInt >= 0 && randomInt <= 1000)
+            {
+               Console.WriteLine(maxValue);
+            }
+            
+
             Console.ReadLine();
         }
     }
